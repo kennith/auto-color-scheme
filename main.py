29e1e7cb541731caa1e40e308bf11eee3fec6a5f
Kennith Leung <kennith.leung@gmail.com>
2020-05-31 11:50:24 -0700

    Starts

---
+++ main.py
@@ -0,0 +1,28 @@
+import sublime
+import sublime_plugin
+from .vendor.darkdetect import isDark, isLight
+
+def plugin_loaded():
+	autodetectCommand = AutodetectCommand(sublime_plugin.TextCommand)
+
+class AutodetectCommand(sublime_plugin.TextCommand):
+	def __init__(self, edit):
+		self.run(edit)
+
+	def run(self, edit):
+		if isDark():
+			self.dark()
+		else:
+			self.light()
+
+	def update(self, scheme):
+		s = sublime.load_settings('Preferences.sublime-settings')
+		s.set("color_scheme", scheme)
+		sublime.save_settings("Preferences.sublime-settings")
+
+	def dark(self):
+		self.update('Packages/auto-detect-color-scheme/themes/GitHub_Dark.tmTheme')
+
+	def light(self):
+		self.update('Packages/auto-detect-color-scheme/themes/GitHub_Light.tmTheme')
+		
